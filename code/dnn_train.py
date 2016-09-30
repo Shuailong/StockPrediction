5f5022ef17798bc05957d10382d2f9c2b4176e03
Liang Shuailong <liangshuailong@gmail.com>
2016-09-30 09:52:57 +0800

    update dnn models

--- code/dnn_train.py
+++ code/dnn_train.py
@@ -12,42 +12,44 @@ Trains and Evaluates the NN using keras.
 
 """
 
+from __future__ import print_function
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 
 def run(dataset):
+
+    batch_size = 16
+    nb_epoch = 20
 
     train_X, train_y = dataset['train']
     dev_X, dev_y = dataset['dev']
     test_X, test_y = dataset['test']
 
-    dimension = len(train_X[0])
-    print 'Input dimension: {}'.format(dimension)
-    print 'Building model...'
+    
+    print('train_X shape:', train_X.shape)
+
+    print('Building model...')
+    
     model = Sequential()
-    model.add(Dense(512, input_dim=dimension, activation='sigmoid'))
-    model.add(Dense(512, activation='sigmoid'))
+    model.add(Dense(1024, input_dim=train_X.shape[1], activation='sigmoid'))
+    model.add(Dropout(0.2))
+    model.add(Dense(1024, activation='sigmoid'))
+    model.add(Dropout(0.2))
     model.add(Dense(2, activation='softmax'))
+
+    model.summary()
 
     model.compile(loss='binary_crossentropy',
                   optimizer='rmsprop',
                   metrics=['accuracy'])
-
-    print 'Training...'
-    model.fit(train_X, train_y, nb_epoch=20, batch_size=16)
-
-    print 'Predicting...'
-    Acc = model.evaluate(train_X, train_y, batch_size=16)
-    print 'Training Data Eval:'
-    print 'Acc: {}%'.format(Acc)
-
-    Acc = model.evaluate(dev_X, dev_y, batch_size=16)
-    print 'Development Data Eval:'
-    print 'Acc: {}%'.format(Acc)
-
-    Acc = model.evaluate(test_X, test_y, batch_size=16)
-    print 'Test Data Eval:'
-    print 'Acc: {}%'.format(Acc)
+    
+    model.fit(train_X, train_y, 
+            batch_size=batch_size, nb_epoch=nb_epoch,
+            verbose=1, validation_data=(dev_X, dev_y))
+    
+    score = model.evaluate(test_X, test_y, verbose=0)
+    print('Test score:', score[0])
+    print('Test accuracy:', score[1])
 
 
 def main():
